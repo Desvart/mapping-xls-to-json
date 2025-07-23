ca0caaf901a2f22c3f83aa674e3e7c6924e9098c
derek <derek.svart_dev@proton.me>
2025-07-23 21:54:38 +0200

    Create CiId object and a usage example in main.py

---
+++ src/CiId.py
@@ -0,0 +1,26 @@
+import re
+
+
+class CiId:
+    def __init__(self, ci_string_id):
+        self.associated_trigram = ""
+        self.name = ""
+        self.code = ""
+        self.parse_properties(ci_string_id)
+
+    def __str__(self):
+        return self.name + " [" + self.code + "]"
+
+    def parse_properties(self, ci_string_id):
+        regexp_pattern = r'^(.*)\s\[(.*)\]$'
+        match = re.match(regexp_pattern, ci_string_id)
+
+        if match:
+            self.name = match.group(1)
+            self.code = match.group(2)
+        else:
+            raise ValueError("Input string does not match the expected format.")
+
+    @property
+    def string_id(self):
+        return self.name + " [" + self.code + "]"
--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+from src.CiId import CiId
+from src.ProductId import ProductId
+
 
 def extract_data_from_excel(file_path):
     faulty_product_table = pd.read_excel(file_path, sheet_name='Sheet1', index_col=0)
@@ -14,16 +17,27 @@ def print_extracted_data(faulty_product_table, faulty_ci_table, git_sonar_table)
     print(git_sonar_table.head())
 
 
-
-def create_objects_from_date()
-
-
 def main():
 
     file_path = '../data/mapping.xlsx'
+    header_product_str_id = "Product String Identifier"
+    header_trigram = "Trigram"
+    header_ci_str_id = "CI"
+    header_git_repo = "Git Repo"
+    header_sonar_url = "SonarQube URL"
 
-    faulty_product_df, faulty_ci_df, git_sonar_df = extract_data_from_excel(file_path)
-    print_extracted_data(faulty_product_df, faulty_ci_df, git_sonar_df)
+    product_str_id_df, ci_str_id_df, git_sonar_df = extract_data_from_excel(file_path)
+    print_extracted_data(product_str_id_df, ci_str_id_df, git_sonar_df)
+
+    product_str_id1 = product_str_id_df[header_product_str_id].iloc[0]
+    product_id1 = ProductId(product_str_id1)
+    print(product_id1)
+    print(product_id1.string_id)
+
+    ci_str_id1 = ci_str_id_df[header_ci_str_id].iloc[0]
+    ci_id1 = CiId(ci_str_id1)
+    print(ci_id1)
+    print(ci_id1.string_id)
 
     # object_list = create_objects_from_data(faulty_product_df, faulty_ci_df, git_sonar_df)
 
