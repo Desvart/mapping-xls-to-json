da4b70ccefc7a9dac9377582ac06f637dae5455a
derek <derek.svart_dev@proton.me>
2025-07-23 21:48:41 +0200

    Add string_id property

--- src/ProductId.py
+++ src/ProductId.py
@@ -1,22 +1,28 @@
 import re
 
 
-class FaultyProduct:
+class ProductId:
     def __init__(self, product_string_id):
-        self.ea_code, self.product_name, self.trigram = self.extract_ea_code(product_string_id)
+        self.ea_code = ""
+        self.name = ""
+        self.trigram = ""
+        self.parse_properties(product_string_id)
+
 
     def __str__(self):
-        return self.trigram + " - " + self.product_name + " [" + self.ea_code + " ]"
+        return self.trigram + " - " + self.name + " [" + self.ea_code + "]"
 
-    def extract_ea_code(self, product_string_id):
-        regexp_pattern = r'^(\w{3})\s-\s([^-]+)\s-\s(\w+)\s\[\1\]$'
+    def parse_properties(self, product_string_id):
+        regexp_pattern = r'^(\w{3})\s-\s(.*)\s-\s(EA\d+)\s\[.*\]$'
         match = re.match(regexp_pattern, product_string_id)
 
         if match:
-            trigram = match.group(1)
-            name = match.group(2).strip()
-            unique_id = match.group(3)
-            return trigram, name, unique_id
+            self.trigram = match.group(1)
+            self.name = match.group(2)
+            self.ea_code = match.group(3)
         else:
             raise ValueError("Input string does not match the expected format.")
 
+    @property
+    def string_id(self):
+        return self.trigram + " - " + self.name + " [" + self.ea_code + "]"
